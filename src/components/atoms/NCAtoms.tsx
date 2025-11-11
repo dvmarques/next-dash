e4a7fd27a9abe3e6b7cd82697d4fd32e50ddf3b6
dvmarques <44673315+dvmarques@users.noreply.github.com>
2025-11-11 13:51:27 -0300

    Adapt login and register page to nextjs (#2)
    * Adapt login and register page to nextjs
    
    * Adapt login and register page to nextjs
    
    * Adapt login and register page to nextjs
    
    * Adapt login and register page to nextjs

--- src/components/atoms/NCAtoms.tsx
+++ src/components/atoms/NCAtoms.tsx
@@ -1,15 +1,44 @@
 "use client";
 
+import CIcon from "@coreui/icons-react";
+import { CButton, CFormInput, CInputGroup, CInputGroupText } from "@coreui/react";
+
 /**
  * Envelopa os  componentes do CoreUI em uma camada de "use client" quando necessário.
  */
-
-import { CButton } from "@coreui/react";
 
 export function NCButton({ children, ...props }: React.ComponentProps<typeof CButton>) {
 	return (
 		<CButton {...props}>
 			{children}
 		</CButton>
+	);
+}
+
+export function NCFormInput({ ...props }: React.ComponentProps<typeof CFormInput>) {
+	return (
+		<CFormInput {...props} />
+	);
+}
+
+export function NCIcon({ ...props }: React.ComponentProps<typeof CIcon>) {
+	return (
+		<CIcon {...props} suppressHydrationWarning={true} />
+	);
+}
+
+export function NCInputGroup({ children, ...props }: React.ComponentProps<typeof CInputGroup>) {
+	return (
+		<CInputGroup {...props}>
+			{children}
+		</CInputGroup>
+	);
+}
+
+export function NCInputGroupText({ children, ...props }: React.ComponentProps<typeof CInputGroupText>) {
+	return (
+		<CInputGroupText {...props}>
+			{children}
+		</CInputGroupText>
 	);
 }
